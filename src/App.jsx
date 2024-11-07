1203a2bfb77c49cd7cf692e76cbbadb0d2fc3079
brian <yangbri1@msu.edu>
2024-11-07 16:22:41 -0500

    setting up form input

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,38 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
+    
     <>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+      <h1>Create Todo List</h1>
+      <form action=""></form>
+      {/* <label> wrapper around <input> auto focus to input when label is clicked  */}
+      <label><b>Task: </b>
+        <input type="text" placeholder="Add task" name="task"/>
+      </label>
+
+      <br />
+
+      <label>
+        <input type="checkbox" defaultChecked={true} name="mockup" />
+        Create Mockup
+      </label>
+
+      <br />
+
+      <label>
+        <input type="checkbox" name="static_layout" />
+        Create Static Layout
+      </label>
+
+      <br />
+
+      <label>
+        <input type="checkbox" name="interactivity" />
+        <input type="text" defaultValue={"Add interactivity"} name="interactivity" />
+      </label>
+      
     </>
+    
   )
 }
 
