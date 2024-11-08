b48f9829e84c3295d8dfd793c5c6f2292415453c
brian <yangbri1@msu.edu>
2024-11-07 22:52:57 -0500

    can now update 'task' variable via setTask function in input form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
+// import useState() to contain form data, useReducer() hook for complex (nested) change in state
 import { useState, useReducer } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 // reducer() function called by dispatch to return new updated state using passed action
@@ -10,51 +9,52 @@ function reducer(state, action){
 
 function App() {
   // call useState() React hook at top level of component to declare state variable -- collect form data
-  const [task, setTask] = useState({
-    // initial placeholders
-    "id": 1,
-    "title": "do the laundry",
-    "completed": false,
-  });
+  const [task, setTask] = useState("");
 
-  // state of
-  const [state, dispatch] = useReducer(reducer)
+  // state parameter usually refers to given initial value (here it's [])
+  // dispatch invokes reducer() function
+  // reducer -> reducer() fn to change state
+  // initial value of empty array []
+  const [state, dispatch] = useReducer(reducer, []);
 
   return (
-    
     <>
-      <h1>Create Todo List</h1>
-      <form action=""></form>
-      {/* <label> wrapper around <input> auto focus to input when label is clicked  */}
-      {/* Aside: "name" attribute provides */}
-      <label><b>Task: </b>
-        <input type="text" placeholder="Add task" name="task"/>
-        <input type="submit" />
-      </label>
-
-      <br />
-
-      <label>
-        <input type="checkbox" defaultChecked={true} name="mockup" />
-        Create Mockup
-      </label>
-
-      <br />
-
-      <label>
-        <input type="checkbox" name="static_layout" />
-        Create Static Layout
-      </label>
-
-      <br />
-      
-      {/* label directs to "interactivity" id -- equivalent to wrapping ex. above*/}
-      <label for="interactivity"></label>
-        <input type="checkbox" name="interactivity" />
-        <input type="text" defaultValue={"Add interactivity"} name="interactivity" />
-      
-      
+      <form>
+        <input type="text"  value={task} onChange={e => setTask(e.target.value)}  />
+      </form>
     </>
+    // <>
+    //   <h1>Create Todo List</h1>
+    //   <form action=""></form>
+    //   {/* <label> wrapper around <input> auto focus to input when label is clicked  */}
+    //   {/* Aside: "name" attribute provides */}
+    //   <label><b>Task: </b>
+    //     <input type="text" placeholder="Add task" name="task"/>
+    //     <input type="submit" />
+    //   </label>
+
+    //   <br />
+
+    //   <label>
+    //     <input type="checkbox" defaultChecked={true} name="mockup" />
+    //     Create Mockup
+    //   </label>
+
+    //   <br />
+
+    //   <label>
+    //     <input type="checkbox" name="static_layout" />
+    //     Create Static Layout
+    //   </label>
+
+    //   <br />
+      
+    //   {/* label directs to "interactivity" id -- equivalent to wrapping ex. above*/}
+    //   <label for="interactivity"></label>
+    //     <input type="checkbox" name="interactivity" />
+    //     <input type="text" defaultValue={"Add interactivity"} name="interactivity" />
+      
+    // </>
 
   )
 }
