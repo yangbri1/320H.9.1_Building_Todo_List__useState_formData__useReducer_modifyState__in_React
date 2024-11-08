e656f95b28507ad7664c5dd37152c6699e351f6e
brian <yangbri1@msu.edu>
2024-11-07 22:43:23 -0500

    incorporating useReducer() hook with todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import { useState } from 'react'
+import { useState, useReducer } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// reducer() function called by dispatch to return new updated state using passed action
+function reducer(state, action){
+
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  // call useState() React hook at top level of component to declare state variable -- collect form data
+  const [task, setTask] = useState({
+    // initial placeholders
+    "id": 1,
+    "title": "do the laundry",
+    "completed": false,
+  });
+
+  // state of
+  const [state, dispatch] = useReducer(reducer)
 
   return (
     
@@ -12,8 +26,10 @@ function App() {
       <h1>Create Todo List</h1>
       <form action=""></form>
       {/* <label> wrapper around <input> auto focus to input when label is clicked  */}
+      {/* Aside: "name" attribute provides */}
       <label><b>Task: </b>
         <input type="text" placeholder="Add task" name="task"/>
+        <input type="submit" />
       </label>
 
       <br />
@@ -31,14 +47,15 @@ function App() {
       </label>
 
       <br />
-
-      <label>
+      
+      {/* label directs to "interactivity" id -- equivalent to wrapping ex. above*/}
+      <label for="interactivity"></label>
         <input type="checkbox" name="interactivity" />
         <input type="text" defaultValue={"Add interactivity"} name="interactivity" />
-      </label>
+      
       
     </>
-    
+
   )
 }
 
