ff087f85e73168ec599e50858c4cd3324413d5ad
brian <yangbri1@msu.edu>
2024-11-09 04:17:01 -0500

    migrated 'checkbox' input form from 'App.jsx' return block over to 'Todo.jsx' component return block -- all previous functionalities still performing but the click 'checkbox' to enable 'Delete' ACTION functionality does NOT seem to want to bind

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -5,6 +5,19 @@ import { ACTION } from "../utilities/reducerActions.mjs"
 export default function Todo({ task, dispatch }){
     return(
         <>  
+            {/* <label> wrapper around <input> form auto focus to input when label is clicked  */}
+            {/* Note: "for" attribute refers to <input>'s id property & provides accessibility for screen readers  */}
+            {/* https://stackoverflow.com/questions/11992026/is-it-better-to-wrap-the-label-tag-around-a-form-item-or-use-the-for-attribute#:~:text=For%20what%20it%27s%20worth%2C%20you%20should%20always%20use,an%20element.%20At%20any%20rate%2C%20it%27s%20good%20practice. */}
+            {/* Note: "name" attribute used when sending data in form submission ... (could act as an identifier too like "id")*/}
+            {/* https://stackoverflow.com/questions/1397592/difference-between-id-and-name-attributes-in-html */}
+            <label for="checkbox_id">
+
+                {/* list of React <input> form props (camelCase): https://react.dev/reference/react-dom/components/input */}
+                {/* Note: "defaultChecked" input props should by default by "false" as set in newTask() helper fn for unfinished tasks,
+                here just dynamically passing it via task.complete */}
+                <input type="checkbox" name="task" id="checkbox_id" defaultChecked={task.complete} onchange={"document.getElementById('delete_btn').disabled = !this.checked;"} />
+              
+            </label>
             {/* utilize ternary operator conditional styling for when todo.complete status is true ... yield green, otw yield red */}
             <span style={{ color: task.complete ?  "rgb(0,250,154)" : "rgb(255,0,40)" }}>
                 {task.title} {" "}
